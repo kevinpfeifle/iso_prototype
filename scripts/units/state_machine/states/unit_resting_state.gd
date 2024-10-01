3e9b20d5a0d542fb1aa9b9019249d494aea6fd96
Kevin Pfeifle <kevinpfeifle@outlook.com>
2024-09-30 22:52:28 -0400

    added a TODO for potential resting state removal

--- scripts/units/state_machine/states/unit_resting_state.gd
+++ scripts/units/state_machine/states/unit_resting_state.gd
@@ -1,6 +1,10 @@
 class_name UnitRestingState
 extends UnitState
 
+# TODO: Determine if this state will ever be needed.
+# In a game where X number of units go and then your turn is over, this state is needed.
+# In a game when player and comp take turns moving a single unit at a time, this state is not needed.
+
 func enter(args):
 	super(args)
 	if unit.animation_player.current_animation != "idle":
